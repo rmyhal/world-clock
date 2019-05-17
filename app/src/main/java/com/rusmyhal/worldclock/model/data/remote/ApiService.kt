bcecf3e706c7ad4501ecaafbb264516bd6b394b6
ruslanmyhal <migalruslann@gmail.com>
2019-05-17 13:02:44 +0300

    create apiService

---
+++ app/src/main/java/com/rusmyhal/worldclock/model/data/remote/ApiService.kt
@@ -0,0 +1,16 @@
+package com.rusmyhal.worldclock.model.data.remote
+
+import com.rusmyhal.worldclock.model.data.remote.entity.TimeZoneResponse
+import io.reactivex.Single
+import retrofit2.http.GET
+
+interface ApiService {
+
+    @GET("$VERSION/list-time-zone")
+    fun getTimeZones(): Single<TimeZoneResponse>
+
+    companion object {
+
+        const val VERSION = "v2.1"
+    }
+}

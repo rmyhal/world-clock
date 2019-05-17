f6a0bb454831517c738c05def02b930979293bae
ruslanmyhal <migalruslann@gmail.com>
2019-05-17 13:02:23 +0300

    create timeZone entities

---
+++ app/src/main/java/com/rusmyhal/worldclock/model/data/remote/entity/TimeZoneResponse.kt
@@ -0,0 +1,9 @@
+package com.rusmyhal.worldclock.model.data.remote.entity
+
+import com.google.gson.annotations.SerializedName
+
+class TimeZoneResponse(
+    @SerializedName("status") val status: String,
+    @SerializedName("message") val message: String,
+    @SerializedName("zones") val timeZones: List<TimeZone>
+)

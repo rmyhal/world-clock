e50710eeb72feecd6066c6c5980557dbada33955
ruslanmyhal <migalruslann@gmail.com>
2019-05-17 13:03:01 +0300

    create timeZoneRepository

---
+++ app/src/main/java/com/rusmyhal/worldclock/model/repository/TimeZoneRepository.kt
@@ -0,0 +1,16 @@
+package com.rusmyhal.worldclock.model.repository
+
+import com.rusmyhal.worldclock.model.data.remote.ApiService
+import com.rusmyhal.worldclock.model.data.remote.entity.TimeZone
+import com.rusmyhal.worldclock.model.system.SchedulersProvider
+import io.reactivex.Single
+
+class TimeZoneRepository(private val api: ApiService, private val schedulers: SchedulersProvider) {
+
+    fun fetchTimeZones(): Single<List<TimeZone>> {
+        return api.getTimeZones()
+            .observeOn(schedulers.ui)
+            .subscribeOn(schedulers.io)
+            .map { response -> response.timeZones }
+    }
+}

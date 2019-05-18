66a06f17f0b07be23837d500c11c55396fba86ca
ruslanmyhal <migalruslann@gmail.com>
2019-05-18 11:34:47 +0300

    edit timeZones request

--- app/src/main/java/com/rusmyhal/worldclock/model/data/remote/ApiService.kt
+++ app/src/main/java/com/rusmyhal/worldclock/model/data/remote/ApiService.kt
@@ -1,13 +1,18 @@
 package com.rusmyhal.worldclock.model.data.remote
 
+import com.rusmyhal.worldclock.BuildConfig
 import com.rusmyhal.worldclock.model.data.remote.entity.TimeZonesResponse
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
     @GET("$VERSION/list-time-zone")
-    fun getTimeZones(): Single<TimeZonesResponse>
+    fun getTimeZones(
+        @Query("format") format: String = "json",
+        @Query("key") apiKey: String = BuildConfig.TIME_ZONE_API_KEY
+    ): Single<TimeZonesResponse>
 
     companion object {
 

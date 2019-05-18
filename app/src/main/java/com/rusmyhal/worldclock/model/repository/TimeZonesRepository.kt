f7b4191cd7ebe46cacacf32e1e61af5a347a7474
ruslanmyhal <migalruslann@gmail.com>
2019-05-18 11:36:15 +0300

    make repository singleton

--- app/src/main/java/com/rusmyhal/worldclock/model/repository/TimeZonesRepository.kt
+++ app/src/main/java/com/rusmyhal/worldclock/model/repository/TimeZonesRepository.kt
@@ -1,16 +1,33 @@
 package com.rusmyhal.worldclock.model.repository
 
+import com.rusmyhal.worldclock.BuildConfig
 import com.rusmyhal.worldclock.model.data.remote.ApiService
+import com.rusmyhal.worldclock.model.data.remote.RetrofitFactory
 import com.rusmyhal.worldclock.model.data.remote.entity.TimeZone
+import com.rusmyhal.worldclock.model.system.AppSchedulers
 import com.rusmyhal.worldclock.model.system.SchedulersProvider
 import io.reactivex.Single
 
-class TimeZonesRepository(private val api: ApiService, private val schedulers: SchedulersProvider) {
+class TimeZonesRepository private constructor(private val api: ApiService, private val schedulers: SchedulersProvider) {
 
     fun fetchTimeZones(): Single<List<TimeZone>> {
         return api.getTimeZones()
             .observeOn(schedulers.ui)
             .subscribeOn(schedulers.io)
             .map { response -> response.timeZones }
+    }
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: TimeZonesRepository? = null
+
+        fun getInstance() =
+            INSTANCE ?: synchronized(TimeZonesRepository::class.java) {
+                INSTANCE ?: TimeZonesRepository(
+                    RetrofitFactory.createService(ApiService::class.java, BuildConfig.TIME_ZONE_ENDPOINT),
+                    AppSchedulers()
+                ).also { INSTANCE = it }
+            }
     }
 }
